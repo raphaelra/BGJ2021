c7f54f12135ceb6b055881039da72d809dfa913f
pedro <pferndd@gmail.com>
2021-02-17 11:30:10 -0300

    basciCharacter + camerafollow + scene pedro

---
+++ JAM - BGJ2021/Assets/Scripts/Pedro/BasicCharacter.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCharacter : MonoBehaviour
+{
+	float horizontal;
+	float vertical;
+	public float moveSpeed;
+	public float jumpForce;
+	public CharacterController controller;
+
+	private Vector3 moveDirection;
+	public float gravityScale;
+
+	public Animator anim;
+	public Transform pivot;
+	public float rotateSpeed;
+
+	public GameObject playerModel;
+	void Start(){
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update(){
+		horizontal = Input.GetAxis("Horizontal");
+		vertical = Input.GetAxis("Vertical");
+		
+		moveDirection = new Vector3(horizontal * moveSpeed, moveDirection.y, vertical * moveSpeed);
+
+		if(controller.isGrounded){
+			if (Input.GetKeyDown(KeyCode.Space)){
+				moveDirection.y = jumpForce;
+			}
+		}
+
+		moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+		controller.Move(moveDirection * Time.deltaTime);
+
+		if(horizontal != 0 || vertical != 0){
+			transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+			Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+			playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+		}
+
+
+		//anim.SetFloat("Speed", (Mathf.Abs(vertical)) + Mathf.Abs(horizontal));
+
+	}
+}
