1f249606c6cd79122725ff54a14def4a4e2b22cc
felipepalesi <61666026+felipepalesi@users.noreply.github.com>
2021-02-20 19:34:53 -0300

    color shift

---
+++ JAM - BGJ2021/Assets/Scripts/ColorShift.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorShift : MonoBehaviour
+{
+    public Image image;
+    [SerializeField] [Range(0f,3f)] float lerpTime = 1f;
+    [SerializeField] Color[] myColors;
+    int colorIndex = 0;
+    float t = 0f;
+    int len = 0;
+
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        len = myColors.Length;
+    }
+
+    
+    void Update()
+    {
+        image.color = Color.Lerp (image.color, myColors[colorIndex], lerpTime * Time.deltaTime);
+
+        t = Mathf.Lerp(t, 1f, lerpTime * Time.deltaTime);
+        if(t>.9f)
+        {
+            t = 0f;
+            colorIndex++;
+            colorIndex = (colorIndex >= len) ? 0 : colorIndex;
+        }
+    }
+}
